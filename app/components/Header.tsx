bd51b75f4e10320c6da655e1621edbf19edee0a1
hrithik210 <hrithiknayak7829@gmail.com>
2024-11-24 12:34:32 +0530

    bit of ui change

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,20 +8,18 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div>
-      <header className="p-4 border-b border-border">
-        <nav className="container mx-auto flex justify-between items-center">
-          <h1 className="text-2xl font-bold">iSwap</h1>
-          <Button asChild variant="outline" className="text-black">
+     <header className="p-4 border-b border-gray-800">
+      <nav className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-2xl font-bold">ISwap</Link>
+        <div className="flex items-center space-x-4">
+          <Button asChild variant="outline" className="bg-black text-white">
             <Link href="/swap">Launch App</Link>
           </Button>
-
-    
-            <WalletMultiButton />
-            <WalletDisconnectButton />
-
-
-        </nav>
-      </header>
+          <WalletMultiButton />
+          <WalletDisconnectButton />
+        </div>
+      </nav>
+    </header>
     </div>
   )
 }
