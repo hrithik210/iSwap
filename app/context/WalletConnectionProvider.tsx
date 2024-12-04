33f60cafddfbf9f736e1cc4df1476f2a44fd5dec
Shri <shricse07@gmail.com>
2024-12-05 01:08:10 +0530

    fix: hydration error

--- app/context/WalletConnectionProvider.tsx
+++ app/context/WalletConnectionProvider.tsx
@@ -1,17 +1,23 @@
 "use client";
 
 import React, { FC, useMemo } from 'react';
+import dynamic from 'next/dynamic';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
-import {
-    WalletModalProvider,
-    WalletDisconnectButton,
-    WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
+//import {
+//    WalletModalProvider,
+//    WalletDisconnectButton,
+//    WalletMultiButton
+//} from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 
 import '@solana/wallet-adapter-react-ui/styles.css';
+
+const WalletModalProvider = dynamic(
+    async () => (await import('@solana/wallet-adapter-react-ui')).WalletModalProvider,
+    { ssr: false }
+);
 
 
 
